a07587543b2b4e01536543248a801c1ffeb69acb
Huy Nguyen <qhuy1911@gmail.com>
2026-07-02 15:51:59 +0700

    fix: add getLoggerId function

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,4 +1,5 @@
 export abstract class LoggerService {
+  abstract getLoggerId(): string;
   abstract log(message: string): void;
   abstract warn(message: string): void;
   abstract error(message: string): void;
